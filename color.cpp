af230a74bdd2e17eef182c72de16a22cdd2104d6
swoyamrajbhandari <156703780+swoyamrajbhandari@users.noreply.github.com>
2024-05-09 20:44:05 -0700

    Update color.cpp

--- color.cpp
+++ color.cpp
@@ -1,33 +1,6 @@
-//[header]
 // Simple example that demonstrates how to convert a color defined as a spectrum to RGB
-//[/header]
-//[compile]
-// Download the mcbeth.cpp file to a folder.
-// Open a shell/terminal, and run the following command where the files is saved:
-//
-// clang++ -o mcbeth mcbeth.cpp -std=c++11 -O3
-//
-// You can use c++ if you don't use clang++
-//
 // Run with: ./mcbeth. Open the resulting image (ppm) in Photoshop or any program
 // reading PPM files.
-//[/compile]
-//[ignore]
-// Copyright (C) 2016  www.scratchapixel.com
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//[/ignore]
 
 #include <fstream>
 #include <iostream>
